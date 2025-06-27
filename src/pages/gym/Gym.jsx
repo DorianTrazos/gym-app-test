11846b8a08208d53f17e51426013a7ad19f00705
DorianTrazos <adrianheras@profesorestrazos.net>
2025-06-27 14:46:50 +0200

    update menu

--- src/pages/gym/Gym.jsx
+++ src/pages/gym/Gym.jsx
@@ -1,12 +1,24 @@
 import Menu from '../../components/menu/Menu';
-import SelectDay from '../../components/select-day/SelectDay';
-
+import styles from './gym.module.css';
 const Gym = () => {
 	return (
 		<>
 			<Menu />
 			<h1>GYM</h1>
-			<SelectDay />
+			<div className={styles.exercises}>
+				<div className={styles.iconBox}>
+					<img src='/assets/icons/front.png' alt='' />
+					<h2>Pecho, triceps, hombro</h2>
+				</div>
+				<div className={styles.iconBox}>
+					<img src='/assets/icons/back.png' alt='' />
+					<h2>Espalda, biceps, antebrazo</h2>
+				</div>
+				<div className={styles.iconBox}>
+					<img src='/assets/icons/leg.png' alt='' />
+					<h2>Pierna</h2>
+				</div>
+			</div>
 		</>
 	);
 };
