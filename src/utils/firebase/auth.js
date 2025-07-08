1c7032fc6f0f010fe3150405153abd0ac4ac6d8e
Dorian <117446752+DorianTrazos@users.noreply.github.com>
2025-07-08 12:42:46 +0200

    Update auth.js

--- src/utils/firebase/auth.js
+++ src/utils/firebase/auth.js
@@ -1,11 +1,18 @@
 import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
-import { auth } from '../../lib/config/firebase.config';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { ROUTINE_INFO } from '../../constants/routines';
+import {
+	auth,
+	exercisesCollectionReference
+} from '../../lib/config/firebase.config';
 
 export const loginUser = async () => {
 	const provider = new GoogleAuthProvider();
 
 	try {
-		await signInWithPopup(auth, provider);
+		const result = await signInWithPopup(auth, provider);
+		const user = result.user;
+		await initializeUserRoutineIfNeeded(user.uid);
 	} catch (error) {
 		console.error('Error al iniciar sesión:', error.code, error.message);
 	}
@@ -17,5 +24,23 @@ export const logoutUser = async () => {
 		console.log('Usuario ha cerrado sesión');
 	} catch (error) {
 		console.error('Error al cerrar sesión:', error.code, error.message);
+	}
+};
+
+const initializeUserRoutineIfNeeded = async userId => {
+	try {
+		const userDocRef = doc(exercisesCollectionReference, userId);
+		const docSnap = await getDoc(userDocRef);
+
+		if (!docSnap.exists()) {
+			await setDoc(userDocRef, {
+				routine: ROUTINE_INFO
+			});
+			console.log(`Rutina inicial guardada para el usuario ${userId}`);
+		} else {
+			console.log('El usuario ya tiene una rutina. No se sobreescribe.');
+		}
+	} catch (error) {
+		console.error('Error al inicializar rutina:', error);
 	}
 };
